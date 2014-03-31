399520d9f3129a3fa81ea2e1bcabd0bb10aa5408
Amin Bandali <me@aminbandali.com>
2014-03-31 19:35:19 -0400

    hasSameStructureAs method now working

--- a10/BinaryTree.java
+++ a10/BinaryTree.java
@@ -24,6 +24,39 @@ public class BinaryTree {
 	}
 	
 	public boolean hasSameStructureAs(BinaryTree tree) {
-		return false;
+//		if (!(getLeftChild() == null || tree.getLeftChild() == null) && !(getLeftChild().hasSameStructureAs(tree.getLeftChild())))
+//			return false;
+//		
+//		
+//		if (!(getRightChild() == null || tree.getRightChild() == null))
+//			if (!(getRightChild().hasSameStructureAs(tree.getRightChild())))
+//				return false;
+//			else
+//				return true;
+//		else if (getRightChild() == null && tree.getRightChild() == null)
+//				return true;
+//		else
+//			return false;
+		
+
+		if(getLeftChild() == null && tree.getLeftChild() == null &&
+			getRightChild() == null && tree.getRightChild() == null )
+	            return true;
+
+		if(!(getLeftChild() == null || tree.getLeftChild() == null) &&
+			getRightChild() == null && tree.getRightChild() == null )
+	            return getLeftChild().hasSameStructureAs(tree.getLeftChild());
+		
+		if(getLeftChild() == null && tree.getLeftChild() == null &&
+	        !(tree.getRightChild() == null || this.getRightChild() == null) )
+	            return getRightChild().hasSameStructureAs(tree.getRightChild());
+
+	    if(!(getLeftChild() == null || tree.getLeftChild() == null) &&
+	    		!(getRightChild() == null || tree.getRightChild() == null) )
+	            return getLeftChild().hasSameStructureAs(tree.getLeftChild()) &&
+	            		getRightChild().hasSameStructureAs(tree.getRightChild());
+
+	    return false;
+					
 	}
 }
